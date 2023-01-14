3b8063db1ad99aa531706e0092a68270ef7bcb35
MatsMardaga <104127794+MatsMardaga@users.noreply.github.com>
2023-01-15 00:05:08 +0100

    can now insert data into database

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 var express = require('express');
+const { createConnection } = require('mysql');
 var router = express.Router();
 
 const db = require('../database');
 
 /* GET users listing. */
-router.get('/showusers', function(req, res) {
+router.get('/showusers', (req, res)=> {
   db.getConnection((err, connection) => {
     if (err) {
       console.log(err);
@@ -14,9 +15,34 @@ router.get('/showusers', function(req, res) {
       connection.query(sql, (err, result)=> {
         if (err) console.log(err);
         res.send(result);
+        connection.release();
       });
     }
   });
+  
 });
+
+router.post('/createuser', (req, res) => {
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      var sql = 'INSERT INTO user(name,email,password) VALUES (?, ?, ?)';
+      var values = [req.query.name, req.query.email, req.query.password];
+      connection.query(sql, values, (err, result) => {
+        if (err) console.log(err);
+        res.send('created a new user');
+        connection.release();
+      }); 
+    }
+  });
+
+});
+
+
+
+
+
 
 module.exports = router;

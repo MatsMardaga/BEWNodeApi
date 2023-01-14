c39414cbcfa5753f127f4dce47183e2044bd9864
MatsMardaga <104127794+MatsMardaga@users.noreply.github.com>
2023-01-14 17:13:58 +0100

    added new working route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/Home', function(req, res, next) {
+  res.render('index', { title: 'Test' });
+});
+
 module.exports = router;

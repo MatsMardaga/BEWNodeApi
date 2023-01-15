265fb695ab593637d449eaec4fbcb6b4f8ac6efa
MatsMardaga <104127794+MatsMardaga@users.noreply.github.com>
2023-01-15 01:07:38 +0100

    delete a user + validation if exists

--- routes/users.js
+++ routes/users.js
@@ -4,24 +4,26 @@ var router = express.Router();
 
 const db = require('../database');
 
-/* GET users listing. */
-router.get('/showusers', (req, res)=> {
+// -------------------------------------------------------Show all users-------------------------------------------------------
+router.get('/showusers', (req, res) => {
   db.getConnection((err, connection) => {
     if (err) {
       console.log(err);
     }
     else {
       var sql = 'SELECT * FROM user';
-      connection.query(sql, (err, result)=> {
+      connection.query(sql, (err, result) => {
         if (err) console.log(err);
         res.send(result);
         connection.release();
       });
     }
   });
-  
+
 });
 
+
+// -------------------------------------------------------Create new user-------------------------------------------------------
 router.post('/createuser', (req, res) => {
   db.getConnection((err, connection) => {
     if (err) {
@@ -35,11 +37,12 @@ router.post('/createuser', (req, res) => {
         if (err) console.log(err);
         res.send('created a new user');
         connection.release();
-      }); 
+      });
     }
   });
 });
 
+// -------------------------------------------------------Update specific user-------------------------------------------------------
 router.put('/updateuser', (req, res) => {
   db.getConnection((err, connection) => {
     if (err) {
@@ -52,21 +55,48 @@ router.put('/updateuser', (req, res) => {
       connection.query(sql, values, (err, result) => {
         if (err) console.log(err);
         //check if user id exists
-        if(result.affectedRows == 0){
+        if (result.affectedRows == 0) {
           console.log('There is no user with that id')
         }
         //check if there is anything to change
-        else if(result.changedRows == 0){
+        else if (result.changedRows == 0) {
           console.log('Nothing to update')
         }
         //if user id exists, the database entry is updated
-        else{
+        else {
           res.send('user has been updated');
           connection.release();
         }
-      }); 
+      });
     }
   });
+});
+
+// -------------------------------------------------------Delete user-------------------------------------------------------
+router.delete('/deleteuser', (req, res) => {
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      //prepared statement
+      var sql = 'DELETE FROM user WHERE id = ?';
+      var values = [req.query.id];
+      connection.query(sql, values, (err, result) => {
+        if (err) console.log(err);
+        //check if user id exists
+        if (result.affectedRows == 0) {
+          console.log('There is no user with that id')
+        }
+        //if user id exists, the database entry is updated
+        else {
+          res.send('user has been deleted');
+          connection.release();
+        }
+      });
+    }
+  });
+
 });
 
 
